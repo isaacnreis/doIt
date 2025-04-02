93050df205c03ee38b6883b94f3830ad31f4e080
Isaac N Reis <isaac.reis@teknisa.com>
2025-04-01 23:34:13 -0300

    subindo restante dos arquivos

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,6 +1,6 @@
 import request from "supertest";
-import { expect, test } from "vitest";
 import app from "../server";
+import { expect, test } from "vitest";
 
 test("Deve registrar um novo usuário", async () => {
   const res = await request(app)
